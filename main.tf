ca5c5168b48835896d00b5be4bb66cdc0d6957eb
YiFanYang1910 <104471488+YiFanYang1910@users.noreply.github.com>
2022-07-09 00:18:56 +1000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  #define terraform version
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "ap-southeast-2"
+  shared_credentials_files = ["/home/ubuntu/.aws/credentials"]
+  //shared_credentials_files = ["/Users/sjq/.aws/credentials"]
+
+}
+
+provider "aws" {
+  alias = "acm_provider"
+  region = "us-east-1"
+}
+
+module "front_end_terraform"{
+    source = "./modules/front_end"
+    bucket_name = "jamesbookinglet.link"
+    domain_name = "jamesbookinglet.link"
+    #在不同的测试环境下定义不同的S3 bucket name
+    # region = "ap-southeast-2"
+    # environment = terraform.workspace
+    zone_id = "Z07447952DDK3PDNX6NZV"
+    //acm_certificate = var.acm_certificate
+    //certificate_arn = "arn:aws:acm:us-east-1:219392032829:certificate/6d9ab804-b45b-4086-8194-660bbb480572"
+    //certificate_arn1 = "arn:aws:acm:us-east-1:219392032829:certificate/6d9ab804-b45b-4086-8194-660bbb480572"
+}
