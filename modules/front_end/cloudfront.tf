c7f699f920548deabcc9073f036df616f47a68e8
YiFanYang1910 <104471488+YiFanYang1910@users.noreply.github.com>
2022-07-09 00:28:01 +1000

    Update cloudfront.tf

--- modules/front_end/cloudfront.tf
+++ modules/front_end/cloudfront.tf
@@ -52,7 +52,7 @@ resource "aws_cloudfront_distribution" "jamesbookingletlinkcd" {
   }
 
   viewer_certificate {
-    acm_certificate_arn = var.certificate_arn1
+    acm_certificate_arn = var.certificate_arn
     ssl_support_method = "sni-only"
     minimum_protocol_version = "TLSv1.2_2021"
     #cloudfront_default_certificate = true
@@ -109,7 +109,7 @@ resource "aws_cloudfront_distribution" "jamesbookingletsubcd" {
   }
 
   viewer_certificate {
-    acm_certificate_arn = var.certificate_arn1
+    acm_certificate_arn = var.certificate_arn
     ssl_support_method = "sni-only"
     minimum_protocol_version = "TLSv1.2_2021"
     # cloudfront_default_certificate = true
